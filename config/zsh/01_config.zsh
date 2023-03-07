01ed13a147c24dd6e583f4bef39d398d54079774
ndunnett <nathandunnett@me.com>
2023-03-08 01:10:41 +1000

    ZSH_AUTOSUGGEST_MANUAL_REBIND=1

--- config/zsh/01_config.zsh
+++ config/zsh/01_config.zsh
@@ -1,10 +1,13 @@
 #!/bin/zsh
 
 # zsh options
-export EDITOR="nano"
-export TIMEFMT=$'real\t%uE\nuser\t%uU\nsys\t%uS'
+EDITOR="nano"
+TIMEFMT=$'real\t%uE\nuser\t%uU\nsys\t%uS'
 setopt HISTIGNOREDUPS
 setopt HISTIGNORESPACE
+
+# speeds up autosuggest
+ZSH_AUTOSUGGEST_MANUAL_REBIND=1
 
 # add common bin directories to path
 append_path "$HOME/bin" "/usr/local/bin"
