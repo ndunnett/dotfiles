27bfe90ec6ce1404565896814aec9a13ea253663
ndunnett <nathandunnett@me.com>
2023-03-10 03:45:50 +1000

    oops

--- zsh/plugins/init.zsh
+++
@@ -1,5 +0,0 @@
-source /Users/ndunnett/dotfiles/zsh/plugins/romkatv/powerlevel10k/powerlevel10k.zsh-theme
-source /Users/ndunnett/dotfiles/zsh/plugins/zimfw/completion/init.zsh
-source /Users/ndunnett/dotfiles/zsh/plugins/zimfw/ssh/init.zsh
-source /Users/ndunnett/dotfiles/zsh/plugins/zdharma/fast-syntax-highlighting/F-Sy-H.plugin.zsh
-source /Users/ndunnett/dotfiles/zsh/plugins/zsh-users/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
