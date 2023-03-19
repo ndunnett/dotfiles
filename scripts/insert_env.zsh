d3521f843825acf782ac4dc23411604627df8768
ndunnett <nathandunnett@me.com>
2023-03-20 04:14:03 +1000

    don't overwrite $PATH

--- scripts/insert_env.zsh
+++ scripts/insert_env.zsh
@@ -11,14 +11,19 @@ new_line="$key=$value"
 # if .zshenv doesn't exist, create it and insert new variable
 if [[ ! -e $zshenv ]]; then
   touch $zshenv
-  echo $new_line >> $zshenv
+  echo "$new_line" >> $zshenv
   changes_made="yes"
 
 # check if line is already in .zshenv
-elif [[ $(grep $new_line $zshenv) == "" ]]; then
-  # check if variable is already defined and overwrite it, otherwise insert new variable
-  old_line=$(grep "$key=" $zshenv)
-  [[ $old_line == "" ]] && echo $new_line >> $zshenv || sed -i "" -e "s|$old_line|$new_line|g" $zshenv
+elif [[ $(grep "$new_line" $zshenv) == "" ]]; then
+  # if this is a path definition, don't overwrite
+  if [[ "$key" == "path" || "$key" == "PATH" || "$key" == "MANPATH" || "$key" == "INFOPATH" ]]; then
+    echo "$new_line" >> $zshenv
+  else
+    # check if variable is already defined and overwrite it, otherwise insert new variable
+    old_line=$(grep "$key=" $zshenv)
+    [[ "$old_line" == "" ]] && echo "$new_line" >> $zshenv || sed -i "" -e "s|$old_line|$new_line|g" $zshenv
+  fi
   changes_made="yes"
 fi
 
