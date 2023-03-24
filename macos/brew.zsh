8a3264444e95bf3408202642d9eb8c7fb4c361ed
ndunnett <nathandunnett@me.com>
2023-03-25 03:12:35 +1000

    install brew bundle

--- macos/brew.zsh
+++ macos/brew.zsh
@@ -4,15 +4,15 @@ echo "[dotfiles] checking brew installation"
 
 # ensure brew is installed
 if [[ ! -e "/opt/homebrew/bin/brew" ]]; then
-  echo "[dotfiles] brew not installed! starting brew installer..."
-  echo "[dotfiles] elevating to sudo"
+  echo "[dotfiles] Homebrew not installed! starting Homebrew installer..."
+  echo "[dotfiles] elevating to sudo..."
   sudo -v
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   changes_made="yes"
 fi
 
 # add brew paths to .zshenv
-echo "[dotfiles] checking brew paths are in .zshenv"
+echo "[dotfiles] checking Homebrew paths are in .zshenv..."
 brew_paths="$(/opt/homebrew/bin/brew shellenv)"
 brew_paths_array=(${(@s:;:)${brew_paths:gs/export //}})
 for brew_path in $brew_paths_array; do
@@ -20,6 +20,10 @@ for brew_path in $brew_paths_array; do
   value=$(echo $brew_path | cut -f2 -d "=" | xargs)
   zsh "$DOTFILES_HOME/scripts/insert_env.zsh" "$key" "$value" && changes_made="yes"
 done
+
+# install bundle from Brewfile
+echo "[dotfiles] installing Homebrew bundle..."
+brew bundle install --file="$DOTFILES_HOME/macos/Brewfile"
 
 # check for changes and exit
 [[ -v changes_made ]] && exit 0 || exit 1
