edb944dfe3816d2b879c16cf92adb016c227dee9
ndunnett <nathandunnett@me.com>
2024-03-20 10:31:25 +0000

    add more plugins

--- zsh/plugin_repos.zsh
+++ zsh/plugin_repos.zsh
@@ -1,7 +1,11 @@
 export plugin_repos=(
   romkatv/powerlevel10k
+  zsh-users/zsh-completions
+  greymd/docker-zsh-completion
   zimfw/completion
   zimfw/ssh
   zdharma/fast-syntax-highlighting
   zsh-users/zsh-autosuggestions
+  desyncr/auto-ls
+  arzzen/calc.plugin.zsh
 )
